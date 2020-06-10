c726e0202c71d1eb8960abcc41ebdd87fd924421
Magnus Møller <magnus108@me.com>
2020-06-10 18:17:29 +0200

    ggmas

--- src/Lib/Build.hs
+++ src/Lib/Build.hs
@@ -17,6 +17,8 @@ import qualified Lib.Location as Location
 import qualified Lib.Grade as Grade
 import qualified Lib.Dump as Dump
 import qualified Lib.Doneshooting as Doneshooting
+import qualified Lib.Dagsdato as Dagsdato
+import qualified Lib.DagsdatoBackup as DagsdatoBackup
 import qualified Lib.Camera as Camera
 
 import Development.Shake
@@ -43,8 +45,6 @@ opts = shakeOptions
             progressDisplay 0.05 (\s -> return ()) p
 
 
-
-
 mkDoneshootingPath :: Int -> FilePath -> Main.Item -> FilePath
 mkDoneshootingPath index file item =
     Doneshooting.unDoneshooting doneshooting </> location </> extension </> grade </> sessionId ++ "." ++ tea ++ "." ++ shootingId ++ "." ++ photographerId ++ "." ++ no ++ (takeExtension file)
@@ -66,33 +66,53 @@ mkDoneshootingPath index file item =
             tea = Lens.view Photographee.tea photographee
             pad x = strPadLeft '0' 3 (show x)
             no = pad index
-                
-                    {-
 
-mkDoneshootingPathJpg :: Doneshooting -> Photographee -> String -> PR.Photographer -> Session.Session -> Shooting -> String -> Int -> Camera.Camera -> FilePath
-mkDoneshootingPathJpg xxx photographee location photographer session shooting filename index camera = 
-    doneshooting (throw ConfigDoneshootingMissing) (\doneshootingDir -> doneshootingDir </> location </> extension
-            </> "_webshop" </> sessionId ++ "." ++ tea ++ "." ++ shootingId ++ "." ++ (PR._tid photographer) ++ "." ++ (pad $ index + 1) ++ (takeExtension filename)) xxx
+
+mkDoneshootingPathJpg :: Int -> FilePath -> Main.Item -> FilePath
+mkDoneshootingPathJpg index file item =
+    Doneshooting.unDoneshooting doneshooting </> location </> extension </> "_webshop" </> sessionId ++ "." ++ tea ++ "." ++ shootingId ++ "." ++ photographerId ++ "." ++ no ++ (takeExtension file)
         where
-            extension =  Camera.camera "cr3" "cr3" camera
-            tea = _tea photographee
-            grade = _grade photographee 
-            sessionId = show $ Session.toInteger session 
-            shootingId = Session.session  --- this wrongs
-                    (Session.type_ (show $ toInteger shooting) ("3"))
-                    (show $ toInteger shooting) session
+            location = takeBaseName $ Location.unLocationFile $ Lens.view Main.location item
+            session = Lens.view Main.session item
+            sessionId = show $ Session.toInteger session
+
+            camera = Lens.view Main.camera item
+            extension = snd $ Camera.toExtension camera
+            photographer = Lens.view Main.photographer item
+            photographerId = Lens.view Photographer.tid photographer
+            doneshooting = Lens.view Main.doneshooting item
+            shooting = Lens.view Main.shooting item
+            shootingId = show $ Shooting.toInteger shooting
+            grade = Grade.showGrade (Lens.view Main.grades item)
+            photographees = Lens.view Main.photographees item
+            photographee = extract (Photographee.unPhotographees photographees)
+            tea = Lens.view Photographee.tea photographee
             pad x = strPadLeft '0' 3 (show x)
+            no = pad index
 
 
-mkDagsdatoPath :: Dagsdato -> Photographee -> String -> String -> UTCTime -> FilePath
-mkDagsdatoPath xxx photographee location filename time =
-    dagsdato (throw ConfigDagsdatoMissing) (\dagsdatoDir -> dagsdatoDir </>  ( date ++ " - " ++ location )</> grade </> (name ++ " - " ++ tea) </> filename) xxx
+mkDagsdatoPath :: FilePath -> String -> Main.Item -> FilePath
+mkDagsdatoPath file date item = dagsdato </> date ++ " - " ++ location </> grade </> (name ++ " - " ++ tea) </> file
         where
-            tea = _tea photographee
-            name = _name photographee
-            grade = _grade photographee 
-            date = getDate time
-            -}
+            dagsdato = Dagsdato.unDagsdato $ Lens.view Main.dagsdato item
+            location = takeBaseName $ Location.unLocationFile $ Lens.view Main.location item
+            grade = Grade.showGrade (Lens.view Main.grades item)
+            photographees = Lens.view Main.photographees item
+            photographee = extract (Photographee.unPhotographees photographees)
+            tea = Lens.view Photographee.tea photographee
+            name = Lens.view Photographee.name photographee
+
+
+mkDagsdatoBackupPath :: FilePath -> String -> Main.Item -> FilePath
+mkDagsdatoBackupPath file date item = dagsdatoBackup </> date ++ " - " ++ location </> grade </> (name ++ " - " ++ tea) </> file
+        where
+            dagsdatoBackup = DagsdatoBackup.unDagsdatoBackup $ Lens.view Main.dagsdatoBackup item
+            location = takeBaseName $ Location.unLocationFile $ Lens.view Main.location item
+            grade = Grade.showGrade (Lens.view Main.grades item)
+            photographees = Lens.view Main.photographees item
+            photographee = extract (Photographee.unPhotographees photographees)
+            tea = Lens.view Photographee.tea photographee
+            name = Lens.view Photographee.name photographee
 
 
 entry :: Main.Item -> IO ()
@@ -113,162 +133,29 @@ myShake opts time item = shake opts $ do
         let jpg = cr -<.> "jpg"
 
         let doneshootingCr = mkDoneshootingPath index cr item
+        let doneshootingJpg = mkDoneshootingPathJpg index jpg item
 
-        want [doneshootingCr] --, doneshootingJpg, dagsdatoCr, dagsdatoJpg, dagsdatoBackupCr, dagsdatoBackupJpg]
+        let dagsdatoCr = mkDagsdatoPath cr time item
+        let dagsdatoJpg = mkDagsdatoPath jpg time item
+
+        let dagsdatoBackupCr = mkDagsdatoBackupPath cr time item
+        let dagsdatoBackupJpg = mkDagsdatoBackupPath jpg time item
+
+        want [doneshootingCr, doneshootingJpg, dagsdatoCr, dagsdatoJpg , dagsdatoBackupCr, dagsdatoBackupJpg]
 
         doneshootingCr %> copyFile' (root </> cr)
 
-        {-
-        doneshootingJpg %> \f -> do
-            copyFile' jpg f
+        doneshootingJpg %> copyFile' (root </> jpg)
 
-        dagsdatoCr %> \f -> do
-            copyFile' cr f
+        dagsdatoCr %> copyFile' (root </> cr)
 
-        dagsdatoJpg %> \f -> do
-            copyFile' jpg f
+        dagsdatoJpg %> copyFile' (root </> jpg)
 
-        dagsdatoBackupCr %> \f -> do
-            copyFile' cr f
+        dagsdatoBackupCr %> copyFile' (root </> cr)
 
-        dagsdatoBackupJpg %> \f -> do
-            copyFile' jpg f
--}
-            {-
-            let extensionFIXME = Camera.cameras (error "fuck") (\c -> Camera.camera "cr3" "cr3" (focus c)) cameras
-            let doneshootingCr = Camera.cameras (error "FUCK") (\c -> mkDoneshootingPath doneshooting photographee location photographer session shooting (takeFileName cr) index (focus c) -<.> extensionFIXME) cameras
-            let doneshootingJpg = Camera.cameras (error "fuck") (\c -> mkDoneshootingPathJpg doneshooting photographee location photographer session shooting (takeFileName jpg) index (focus c) -<.> "jpg") cameras 
-
-
-            --let doneshootingBackupCr2 = mkDoneshootingPath doneshootingBackup photographee location photographer session shooting (takeFileName cr3) index -<.> "cr3"
-            --let doneshootingBackupJpg = mkDoneshootingPathJpg doneshootingBackup photographee location photographer session shooting (takeFileName jpg) index -<.> "jpg"
-
-            let dagsdatoCr = mkDagsdatoPath dagsdato photographee location (takeFileName cr) time -<.> extensionFIXME
-            let dagsdatoJpg = mkDagsdatoPath dagsdato photographee location (takeFileName jpg) time -<.> "jpg"
-
-            let dagsdatoBackupCr = mkDagsdatoPath dagsdatoBackup photographee location (takeFileName cr) time -<.> extensionFIXME
-            let dagsdatoBackupJpg = mkDagsdatoPath dagsdatoBackup photographee location (takeFileName jpg) time -<.> "jpg"
-
-            want [doneshootingCr, doneshootingJpg, dagsdatoCr, dagsdatoJpg
-                -- , doneshootingBackupCr2, doneshootingBackupJpg, 
-                 , dagsdatoBackupCr, dagsdatoBackupJpg] 
-
-            doneshootingCr %> \f -> do
-                copyFile' cr f
-
-            doneshootingJpg %> \f -> do
-                copyFile' jpg f
-
-            {-
-            doneshootingBackupCr2 %> \f -> do
-                copyFile' cr3 f
-
-            doneshootingBackupJpg %> \f -> do
-                copyFile' jpg f
-            -}
-
-            dagsdatoCr %> \f -> do
-                copyFile' cr f
-
-            dagsdatoJpg %> \f -> do
-                copyFile' jpg f
-
-            dagsdatoBackupCr %> \f -> do
-                copyFile' cr f
-
-            dagsdatoBackupJpg %> \f -> do
-                copyFile' jpg f
-
-
-    return ()
-    -}
+        dagsdatoBackupJpg %> copyFile' (root </> jpg)
 
     {-
-    files <- liftIO $ getDirectoryFilesIO dir1 ["//*.cr2"] 
-
-    let bySys = groupBy (\x y -> toSys (splitDirectories x) == (toSys (splitDirectories y))) files
-
-    forM_ bySys $ \ sys -> do
-        ifor_ sys $ \ index' x -> do
-            let index = index' + 1
-            let doneshooting = toDoneshooting photographer x
-            let cr2 = dir2 </> toCr2 index doneshooting
-            let jpg = dir2 </> toJpg index doneshooting
-            want [cr2, jpg]
-
-            cr2 %> \f -> do 
-                copyFile' (dir1 </> x) f                                                  
-
-            jpg %> \f -> do 
-                copyFile' (dir1 </> x -<.> "jpg") f                                                    
-
-
-
-actions :: ShakeConfig -> Photographee -> String -> UTCTime -> Bool -> Rules ()
-actions config photographee location time removeIt = do
-        --
-        -- badIO
-        dagsdato <- liftIO $ getDagsdato config
-        cameras <- liftIO $ getCameras config
-
-        dump_ <- liftIO $ getDump config 
-        (DumpFiles dumpFiles) <- liftIO $ getDumpFiles dump_ cameras
-
-        dagsdatoBackup <- liftIO $ getDagsdatoBackup config
-        doneshootingBackup <- liftIO $ getDoneshootingBackup config
-        
-        doneshooting <- liftIO $ getDoneshooting config
-        photographer <- liftIO $ getPhotographer config
-        session <- liftIO $ getSession config
-        shooting <- liftIO $ getShooting config
-        -- badIO
-
-        ifor_ (sort dumpFiles) $ \ index (cr, jpg) -> do
-            let extensionFIXME = Camera.cameras (error "fuck") (\c -> Camera.camera "cr3" "cr3" (focus c)) cameras
-            let doneshootingCr = Camera.cameras (error "FUCK") (\c -> mkDoneshootingPath doneshooting photographee location photographer session shooting (takeFileName cr) index (focus c) -<.> extensionFIXME) cameras
-            let doneshootingJpg = Camera.cameras (error "fuck") (\c -> mkDoneshootingPathJpg doneshooting photographee location photographer session shooting (takeFileName jpg) index (focus c) -<.> "jpg") cameras 
-
-
-            --let doneshootingBackupCr2 = mkDoneshootingPath doneshootingBackup photographee location photographer session shooting (takeFileName cr3) index -<.> "cr3"
-            --let doneshootingBackupJpg = mkDoneshootingPathJpg doneshootingBackup photographee location photographer session shooting (takeFileName jpg) index -<.> "jpg"
-
-            let dagsdatoCr = mkDagsdatoPath dagsdato photographee location (takeFileName cr) time -<.> extensionFIXME
-            let dagsdatoJpg = mkDagsdatoPath dagsdato photographee location (takeFileName jpg) time -<.> "jpg"
-
-            let dagsdatoBackupCr = mkDagsdatoPath dagsdatoBackup photographee location (takeFileName cr) time -<.> extensionFIXME
-            let dagsdatoBackupJpg = mkDagsdatoPath dagsdatoBackup photographee location (takeFileName jpg) time -<.> "jpg"
-
-            want [doneshootingCr, doneshootingJpg, dagsdatoCr, dagsdatoJpg
-                -- , doneshootingBackupCr2, doneshootingBackupJpg, 
-                 , dagsdatoBackupCr, dagsdatoBackupJpg] 
-
-            doneshootingCr %> \f -> do
-                copyFile' cr f
-
-            doneshootingJpg %> \f -> do
-                copyFile' jpg f
-
-            {-
-            doneshootingBackupCr2 %> \f -> do
-                copyFile' cr3 f
-
-            doneshootingBackupJpg %> \f -> do
-                copyFile' jpg f
-            -}
-
-            dagsdatoCr %> \f -> do
-                copyFile' cr f
-
-            dagsdatoJpg %> \f -> do
-                copyFile' jpg f
-
-            dagsdatoBackupCr %> \f -> do
-                copyFile' cr f
-
-            dagsdatoBackupJpg %> \f -> do
-                copyFile' jpg f
-
-
         x <- liftIO $ getDump config
         dump (action $ return ()) (\fp -> do
                     liftIO $ setId config Id.noId
